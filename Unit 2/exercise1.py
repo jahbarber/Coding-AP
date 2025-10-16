35d3a29d228d81db62b2ccadb9557b650b0ebe12
S <144710839+jahbarber@users.noreply.github.com>
2025-10-16 15:13:10 +0000

    done

--- Unit 2/exercise1.py
+++ Unit 2/exercise1.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import nfl_data_py as nfl
 
-from helperFunctions import get_team_records
+from helperFunctions import get_team_records, get_season_Results_By_team
 
-schedules = nfl.import_schedules([2023])
+# schedules = nfl.import_schedules([2023])
 
 #print(schedules.columns.tolist())
 
 records = get_team_records(2020)
 
-print(records[['team','points_for','points_against']])
+birds = get_season_Results_By_team(2025,)
+print(birds)
+pd =[ 4, 3, 7, 6, -4, -17]
+pd =[ 25, 1, 21, -7, 34, 4]
+
+# COLTS AND BUCS H
+
+
+
 
 
 # print(records[[' wins']].mean())
---
+++ Unit 2/exercise2.py
@@ -0,0 +1,68 @@
+# import pandas as pd
+# import nfl_data_py as nfl
+
+# from helperFunctions import get_team_records, get_season_Results_By_team
+
+# schedules = get_team_records(2025)
+# print(schedules)
+
+
+
+# top6_Teams = ['TB','IND','LA','BUF','SF','SEA','PIT']
+
+# Which team has the best point differential this season
+# team_1=get_season_Results_By_team(2025,'TB')
+# team_2=get_season_Results_By_team(2025,'IND')
+# team_3=get_season_Results_By_team(2025,'LA')
+# team_4=get_season_Results_By_team(2025,'BUF')
+# team_5=get_season_Results_By_team(2025,'SF')
+# team_6=get_season_Results_By_team(2025,'SEA')
+
+# print(team_1)
+# print(team_2)
+# print(team_3)
+# print(team_4)
+# print(team_5)
+# print(team_6)
+
+# 1:IND=78
+
+# 2:SF=-3
+
+# 3:
+
+# def numbers():
+#     total=0
+# while True:
+#     number_input= input("Enter the numbers (or 'done' to finish):")
+#     if number_input.lower()== 'done':
+#          break  
+#     try:
+#         number = float(number_input)  # Convert input to a float
+#         total += number
+
+#     except ValueError:
+#         print("Invalid input. Please enter a number or 'done'.")
+
+# print(f"The sum of the numbers is: {total}")
+
+
+
+
+
+
+
+def pdCheck():
+    print("please enter a number. Please enter 'q' to calculate")
+    number = int(input)
+    values = []
+    
+    while number != 'stop':.
+    values.append(number)
+    print(values)
+    print("please enter a number")
+    number = input
+    else:
+        print('doing calculation')
+
+pdCheck()
