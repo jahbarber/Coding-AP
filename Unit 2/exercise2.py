35aabb01d9c4072f2cc2b51adb74ad09c48c5b1c
S <144710839+jahbarber@users.noreply.github.com>
2025-10-30 15:08:17 +0000

    Classwork

--- Unit 2/exercise2.py
+++ Unit 2/exercise2.py
@@ -1,35 +1,49 @@
-# import pandas as pd
-# import nfl_data_py as nfl
+import pandas as pd
+import nfl_data_py as nfl
 
-# from helperFunctions import get_team_records, get_season_Results_By_team
+from helperFunctions import get_team_records, get_season_Results_By_team
 
-# schedules = get_team_records(2025)
-# print(schedules)
+schedules = get_team_records(2025)
+print(schedules)
 
 
 
-# top6_Teams = ['TB','IND','LA','BUF','SF','SEA','PIT']
+top6_Teams = ['TB','IND','LA','BUF','SF','SEA','PIT']
 
 # Which team has the best point differential this season
 # team_1=get_season_Results_By_team(2025,'TB')
-# team_2=get_season_Results_By_team(2025,'IND')
+team_2=get_season_Results_By_team(2025,'IND')
 # team_3=get_season_Results_By_team(2025,'LA')
 # team_4=get_season_Results_By_team(2025,'BUF')
 # team_5=get_season_Results_By_team(2025,'SF')
 # team_6=get_season_Results_By_team(2025,'SEA')
 
 # print(team_1)
-# print(team_2)
+print(team_2)
 # print(team_3)
 # print(team_4)
 # print(team_5)
 # print(team_6)
 
+#TB home:7, away:8
+#IND home:64
+#LA home:
+#BUF home:
+#SF home:
+#SEA home:
+
+
+
+
 # 1:IND=78
 
 # 2:SF=-3
 
-# 3:
+# 3. Which team has best home point differential this season ?
+'The best home PD is INDY  64'
+
+# 4. Which team has the best away point differential this season ?
+'The best away PD is '
 
 # def numbers():
 #     total=0
@@ -52,17 +66,28 @@
 
 
 
+from optparse import Values
+
+
 def pdCheck():
-    print("please enter a number. Please enter 'q' to calculate")
-    number = int(input)
+    print("please enter a number")
+    number = input()
     values = []
     
-    while number != 'stop':.
-    values.append(number)
-    print(values)
-    print("please enter a number")
-    number = input
+    while number != 'q':
+        values.append(int(number))
+        print(values)
+        print("please enter a number")
+        number = input()
     else:
-        print('doing calculation')
+        print('doing calculation...') 
+        total = sum(values)
+        print(total)
 
+    # For loop
+    # Add all the numbers and return the sum of the numbers.
+    # this should give us the PD.
+    # HINT: you're going to be using a for loop
+# for value in number:
+    # print('doing calculation...')
 pdCheck()
---
+++ Unit 2/exercise3.py
@@ -0,0 +1,44 @@
+from helperFunctions import weeklyPlayerStats
+
+
+
+
+
+# Copy the new weeklyPlayerStats() function into your helperFunction.py file.
+
+# Create a new python document caled  exercise3.py . Using the individual player helper function, 
+# answer the following questions:
+
+# NOTE = you will need to pass the position, year and week in as an agrument
+# position example : quarterback = 'QB', runnerback = 'RB'
+
+exampleData = weeklyPlayerStats(2024,'WR')
+
+print(exampleData)
+
+# 1. Who are the top 5 quarterbacks by passing yards? What’s their average completion percentage (cmp_pct)?
+#
+
+
+
+# 2. What might a high cmp_pct tell you about a quarterback’s style of play? The they are team player
+# 
+
+# 3. Which RB had the highest rushing yards that week? Who had the best yards per carry (rush_ypc)?
+'Rushing yards - barkley '
+'Yards per carry - henry '
+
+# 4. If a RB has high rush_yards but low rush_ypc, what could that mean?
+
+# 5. Find a player who has the recieving yards on the fewest pass attempts?
+# L. McConkey
+
+
+# import pandas as pd
+# import nfl_data_py as nfl
+
+# from helperFunctions import weeklyPlayerStats
+
+
+# stats = weeklyPlayerStats(2024, 'QB', '1')
+# print(stats)
