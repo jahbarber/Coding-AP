9e560c6aa6cd0f25c44b4ff9b3e3889c4532f01e
S <144710839+jahbarber@users.noreply.github.com>
2025-12-04 15:56:47 +0000

    Work completed

--- Unit 4/visualization.py
+++ Unit 4/visualization.py
@@ -1,13 +1,12 @@
-from helperLogic import plot_position_stat_bar
+from helperLogic import plot_position_stat_bar,  plot_player_stat_by_week
 
-plot_position_stat_bar(2024, 'QB','passing_yards', save_path='Qb_passing_2024.png')
+# plot_position_stat_bar(2024, 'QB','passing_yards', save_path='Qb_passing_2024.png')
+plot_player_stat_by_week(2024, 'Patrick', 'Mahomes', 'passing_yards', save_path='Qb_passing_2024.png')
 1.
-'Josh Jacob 2022'
-'Christian McCaffrey 2023'
-'Saquon Barkley 2024'
+'Derrick Henry had the most rushing yards over 2022-2024'
 
 
 
 
 2.
-'Patrick Mahomes has declined over this period of time'
+'Patrick Mahomes has declined over this period over time'
