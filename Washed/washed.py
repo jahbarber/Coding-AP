99638cd7e3b5b19ac8e8afa123bd5c4ccd4715d0
S <144710839+jahbarber@users.noreply.github.com>
2025-12-05 16:10:11 +0000

    work completed

---
+++ Washed/washed.py
@@ -0,0 +1,19 @@
+from helperLogic import get_season_totals_by_position, plot_position_stat_bar, plot_player_stat_by_week, get_player_stats
+
+'ran through- lost the touch- not how they used to be out- out of prime'
+'this player is on the decline'
+'1. games played - indicates how healthy they are and their availability'
+'2 passing yards - indicates athlecticism'
+'The amount of seasons they played- the longer they play thew more likely theyre to decline'
+
+
+
+# stats = get_player_stats(2024,'Philip', 'Rivers' )
+# print(stats)
+
+
+
+
+
+'Brandon Walton played 1 out of the sevnteen games his team played last season'
+'On average a quarterback should play all 17 games in the season'
