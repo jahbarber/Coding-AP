3e1dc112b79660765deaedfb9477e95c01d7cbff
S <144710839+jahbarber@users.noreply.github.com>
2026-03-13 15:06:07 +0000

    Project done

--- Project/Fan.py
+++ Project/Fan.py
@@ -1,6 +1,6 @@
 class Fan:
     
-    temp_thresholds = [20, 25, 30, 35, 40]
+    temp_thresholds = [75, 85, 90, 95, 100]
     speeds = [0, 1, 2, 3, 4]
 
     def __init__(self, temp):
@@ -10,19 +10,18 @@ class Fan:
         
         if self.current_temp < self.temp_thresholds[0]:
             return self.speeds[0]
-            
-        for i in range(len(self.temp_thresholds)):
-            
-            if self.current_temp < self.temp_thresholds[i]:
-                return self.speeds[i-1]
-        
-       
+        else:
+            for i in range(len(self.temp_thresholds)):
+                if self.current_temp < self.temp_thresholds[i]:
+                    return self.speeds[i-1]
+
         return self.speeds[-1]
 
-# User Input
+
 try:
     temp_input = int(input("Enter the temperature: "))
     my_fan = Fan(temp_input)
     print("The speed of the fan is:", my_fan.get_speed())
 except ValueError:
     print("Please enter a valid integer.")
+
