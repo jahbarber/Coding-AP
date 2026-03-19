6802f678dd891f9ba24a1b4513368b536c2f7e91
S <144710839+jahbarber@users.noreply.github.com>
2026-03-19 15:09:40 +0000

    work done

---
+++ activityMar19.py
@@ -0,0 +1,40 @@
+# In your AP Coding repo, create a new document called activityMar19.py and then copy, paste, and answer the following questions.
+# This activity is due at the end of class. 
+
+# 1. Convert the following binary number into a decimal number: 10110. 22
+
+# 2. Convert the following decimal number into a binary number: 45.                101101                    
+
+# 3. What will the following code block output:5
+# val1 <- 10
+# val2 <- 5
+# val1 <- val1 + val2
+# val2 <- val1 - val2
+# val1 <- val1 - val2
+
+# 4. What will the following code block output: a-b if a>b or b-a
+# PROCEDURE calculate(a, b)
+# {
+#    IF (a > b)
+#    {
+#       RETURN a - b
+#    }
+#    ELSE
+#    {
+#       RETURN b - a
+#    }
+# }
+
+# 5. What will the following code block output: Nothing
+# list1 <- [5, 10, 15]
+# list2 <- []
+# FOR EACH item IN list1
+# {
+#    INSERT(list2, 1, item * 2)
+# }
+# DISPLAY(list2)
+
+# # 6.  A robot is in the center (0,0) of a 5 x 5 grid, facing North. Create a code block that will
+# # move the robot to coordinate 5,5?
+
+# # hint: you may need to use a loop.
